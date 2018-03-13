63b659a18aa7988417e49f344bc5fa5241af2e6a
Mike Kinney <mikeki@hotmail.com>
2018-03-13 10:51:51 -0700

    Removing Exrin Pt. 2 - UnitTests still broken.

--- Instagram/Instagram/Proxy/PageProxy.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using Xamarin.Forms;
-
-namespace Instagram.Proxy
-{
-	/// <summary>
-	/// Implements IView on all ContentPages. ALL your views must inherit from this proxy.
-	/// </summary>
-}
